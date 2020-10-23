8538a1763a16aac5a67c75bf134a1fe6ca0fe325
f4nke <cool-vlad@mail.ru>
2020-10-23 23:21:32 +0300

    2.2.1

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,44 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User getUser(String model, int series) {
+      Query query = sessionFactory.getCurrentSession()
+                                  .createQuery("From Car where model = :paramModel and series = :paramSeries");
+      query.setParameter("paramModel", model);
+      query.setParameter("paramSeries", series);
+
+      Car car = (Car) query.getSingleResult();
+
+      return car.getUser();
+   }
+
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,62 @@
+package hiber.model;
+
+import org.hibernate.mapping.Join;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String model;
+
+    private int series;
+
+    @OneToOne(mappedBy = "car")
+    @PrimaryKeyJoinColumn
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Model - " + getModel() + " Series - " + getSeries();
+    }
+}
